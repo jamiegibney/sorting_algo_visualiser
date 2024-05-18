3f679d48ec0e1a2bc93b6e74964f2e2e2bb174d2
Jamie Gibney <123845103+jamiegibney@users.noreply.github.com>
2024-05-18 10:08:16 +0100

    Refactored envelope.rs

--- src/audio/envelope.rs
+++ src/audio/envelope.rs
@@ -7,14 +7,11 @@ static ENVELOPE_BYTES: &[u8; 134784] = include_bytes!("envelope_data");
 pub struct AmpEnvelope {
     data: &'static [f32],
     read_pos: usize,
-    is_active: bool,
 }
 
 impl AmpEnvelope {
-    /// Creates a new envelope of `len_secs` seconds.
-    pub fn with_length(len_secs: f32, sample_rate: f32) -> Self {
-        let num_samples = (len_secs * sample_rate).ceil() as usize;
-
+    /// Creates a new envelope.
+    pub fn with_length(sample_rate: f32) -> Self {
         let f32_size = std::mem::size_of::<f32>();
 
         Self {
@@ -25,29 +22,24 @@ impl AmpEnvelope {
                 )
             },
             read_pos: 0,
-            is_active: true,
         }
     }
 
     /// Returns the next envelope sample. Returns `None` if the envelope has reached
     /// its end point.
     pub fn next(&mut self) -> Option<f32> {
-        if !self.is_active {
+        if !self.is_active() {
             return None;
         }
 
         let pos = self.read_pos;
         self.read_pos += 1;
 
-        if self.read_pos == self.data.len() {
-            self.is_active = false;
-        }
-
         Some(self.data[pos])
     }
 
     /// Whether the envelope is active or not.
     pub const fn is_active(&self) -> bool {
-        self.is_active
+        self.read_pos < self.data.len()
     }
 }
