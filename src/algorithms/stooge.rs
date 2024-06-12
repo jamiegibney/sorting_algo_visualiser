14148928be1b2afe8c543b9e33e3b3b59bde1a89
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-12 03:50:17 +0100

    updated stooge sort

--- src/algorithms/stooge.rs
+++ src/algorithms/stooge.rs
@@ -1,16 +1,34 @@
 use super::*;
 
 #[derive(Debug)]
-pub struct Stooge {}
+pub struct Stooge;
 
 impl Stooge {
-    pub fn new() -> Self {
-        Self {}
+    pub const fn new() -> Self {
+        Self
+    }
+
+    fn sort(arr: &mut SortArray, l: usize, h: usize) {
+        if l >= h {
+            return;
+        }
+
+        if arr.cmp(l, h, Ordering::Less) {
+            arr.swap(l, h);
+        }
+
+        if h - l + 1 > 2 {
+            let t = (h - l + 1) / 3;
+
+            Self::sort(arr, l, h - t);
+            Self::sort(arr, l + t, h);
+            Self::sort(arr, l, h - t);
+        }
     }
 }
 
 impl SortAlgorithm for Stooge {
     fn process(&mut self, arr: &mut SortArray) {
-        //
+        Self::sort(arr, 0, arr.len() - 1);
     }
 }
