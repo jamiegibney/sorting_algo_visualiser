d19f4a4d7c89e65b49d6730765c15af79e29a4fc
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-12 03:01:28 +0100

    implemented comb sort

--- src/algorithms/comb.rs
+++ src/algorithms/comb.rs
@@ -1,16 +1,34 @@
 use super::*;
 
 #[derive(Debug)]
-pub struct Comb {}
+pub struct Comb;
 
 impl Comb {
-    pub fn new() -> Self {
-        Self {}
+    pub const fn new() -> Self {
+        Self
+    }
+
+    fn next_gap(gap: usize) -> usize {
+        (gap * 10 / 13).max(1)
     }
 }
 
 impl SortAlgorithm for Comb {
     fn process(&mut self, arr: &mut SortArray) {
-        //
+        let n = arr.len();
+        let mut gap = n;
+        let mut swapped = true;
+
+        while gap != 1 || swapped {
+            gap = Self::next_gap(gap);
+            swapped = false;
+
+            for i in 0..(n - gap) {
+                if arr.cmp(i, i + gap, Ordering::Greater) {
+                    arr.swap(i, i + gap);
+                    swapped = true;
+                }
+            }
+        }
     }
 }
