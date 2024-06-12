f39b11f4f1d93199e787e4dbf83814fe5cf6d787
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-12 03:03:13 +0100

    implemented cocktail sort

--- src/algorithms/cocktail.rs
+++ src/algorithms/cocktail.rs
@@ -11,6 +11,37 @@ impl Cocktail {
 
 impl SortAlgorithm for Cocktail {
     fn process(&mut self, arr: &mut SortArray) {
-        //
+        let n = arr.len();
+
+        let mut swapped = true;
+        let mut start = 0;
+        let mut end = n - 1;
+
+        while swapped {
+            swapped = false;
+
+            for i in start..end {
+                if arr.cmp(i, i + 1, Ordering::Greater) {
+                    arr.swap(i, i + 1);
+                    swapped = true;
+                }
+            }
+
+            if !swapped {
+                break;
+            }
+
+            swapped = false;
+            end -= 1;
+
+            for i in (start..end).rev() {
+                if arr.cmp(i, i + 1, Ordering::Greater) {
+                    arr.swap(i, i + 1);
+                    swapped = true;
+                }
+            }
+
+            start += 1;
+        }
     }
 }
