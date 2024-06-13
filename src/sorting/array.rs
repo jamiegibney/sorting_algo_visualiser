15cbbb64e76e580e571cb3744b775544c9adfbef
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-14 00:01:00 +0100

    refactored sorting array file

--- src/sorting/array.rs
+++ src/sorting/array.rs
@@ -14,17 +14,17 @@ pub struct SortArray {
     /// The current sorting algorithm.
     curr_algorithm: SortingAlgorithm,
 
-    /// The scratch buffer, used for the sorting process.
+    /// The "sorting array", used for the sorting process.
     arr: Vec<usize>,
-    /// The initial array before the sorting process.
+    /// The initial array state before any sorting process.
     initial_arr: Vec<usize>,
 
-    /// The buffer of operations — where the sorting operations are recorded
-    /// to.
+    /// The buffer of operations, i.e. where the sorting operations are
+    /// recorded to.
     op_buffer: Vec<SortOperation>,
 
-    /// A counter which is passed to a capture to prevent unnecessary
-    /// computation later.
+    /// A counter which is passed to created `SortCapture`s to prevent
+    /// unnecessary computation later.
     num_writes: usize,
 }
 
@@ -39,30 +39,83 @@ impl SortArray {
         }
     }
 
-    /// Writes `value` to position `idx`. Will panic if
-    /// `idx > `[`SortArray::len()`].
+    /// Writes `value` to position `idx` in the array.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if `idx >= `[`SortArray::len()`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut arr = SortArray(4); // start with [0, 1, 2, 3]
+    ///
+    /// arr.write(2, 42); // set to [0, 1, 42, 3]
+    /// assert_eq!(arr.read(2), 42);
+    /// arr.write(3, 0); // set to [0, 1, 42, 0]
+    /// assert_eq!(arr.read(3), 0);
+    /// ```
     pub fn write(&mut self, idx: usize, value: usize) {
         self.push(SortOperation::Write { idx, value });
         self.arr[idx] = value;
         self.num_writes += 1;
     }
 
-    /// Returns the value as position `idx`. Will panic if
-    /// `idx > `[`SortArray::len()`].
+    /// Returns the value at position `idx` in the array.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if `idx >= `[`SortArray::len()`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut arr = SortArray(3); // start with [0, 1, 2]
+    /// arr.swap(0, 2); // set to [2, 1, 0];
+    /// assert_eq!(arr.read(0), 2);
+    /// assert_eq!(arr.read(2), 0);
+    /// ```
     pub fn read(&mut self, idx: usize) -> usize {
         self.push(SortOperation::Read { idx });
         self.arr[idx]
     }
 
-    /// Swaps the elements at positions `a` and `b`. Will panic if either index
-    /// is greater than [`SortArray::len()`].
+    /// Swaps the elements at positions `a` and `b` in the array.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if either `a` or `b` is greater than or equal to
+    /// [`SortArray::len()`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut arr = SortArray::new(3); // start with [0, 1, 2]
+    /// arr.swap(0, 2); // sets to [2, 1, 0]
+    /// arr.swap(1, 0); // sets to [1, 2, 0]
+    /// ```
     pub fn swap(&mut self, a: usize, b: usize) {
         self.push(SortOperation::Swap { a, b });
         self.arr.swap(a, b);
     }
 
-    /// Compares the elements at positions `a` and `b` to match `ord`. Will
-    /// panic if either index is greater than [`SortArray::len()`].
+    /// Returns `true` if the comparison of elements `a` and `b` matches `ord`.
+    ///
+    /// # Panics
+    ///
+    /// Will panic if either `a` or `b` is greater than or equal to
+    /// [`SortArray::len()`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// let mut arr = SortArray::new(5); // start with [0, 1, 2, 3, 4]
+    /// arr.write(4, 2); // set to [0, 1, 2, 3, 2]
+    ///
+    /// assert!(arr.cmp(0, 1, Ordering::Less)); // arr[0] < arr[1]
+    /// assert!(arr.cmp(2, 4, Ordering::Equal)); // arr[2] == arr[4]
+    /// assert!(arr.cmp(3, 1, Ordering::Greater)); // arr[3] > arr[1]
+    /// ```
     pub fn cmp(&mut self, a: usize, b: usize, ord: Ordering) -> bool {
         let cmp = self.arr[a].cmp(&self.arr[b]);
         let res = cmp == ord;
@@ -70,6 +123,11 @@ impl SortArray {
         self.push(SortOperation::Compare { a, b, res });
 
         res
+    }
+
+    /// The number of elements in the array.
+    pub fn len(&self) -> usize {
+        self.arr.len()
     }
 
     /// Copies the internal array to `dest`.
@@ -86,6 +144,8 @@ impl SortArray {
 
         dest.copy_from_slice(&self.arr);
     }
+
+    // MANAGEMENT METHODS
 
     /// Prepares the array for sorting, using its current state as the initial
     /// array.
@@ -141,11 +201,6 @@ impl SortArray {
     /// Whether the array is currently sorted.
     pub fn is_sorted(&self) -> bool {
         self.arr.iter().enumerate().all(|(i, &v)| i == v)
-    }
-
-    /// The number of elements in the array.
-    pub fn len(&self) -> usize {
-        self.arr.len()
     }
 
     /// Returns the array as a slice.
