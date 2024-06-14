834752a47617f65c8d5535cc3f74f607f33b812e
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-14 04:04:37 +0100

    implemented timsort

--- src/algorithms/timsort.rs
+++ src/algorithms/timsort.rs
@@ -1,16 +1,39 @@
 use super::*;
 
 #[derive(Debug)]
-pub struct Timsort;
+pub struct Timsort {
+    merge: Merge,
+}
 
 impl Timsort {
-    pub const fn new() -> Self {
-        Self
+    const RUN: usize = 32;
+
+    pub fn new() -> Self {
+        Self { merge: Merge::new() }
     }
 }
 
 impl SortProcessor for Timsort {
     fn process(&mut self, arr: &mut SortArray) {
-        unimplemented!();
+        let n = arr.len();
+
+        for i in (0..n).step_by(Self::RUN) {
+            let right = usize::min(i + Self::RUN - 1, n - 1);
+            super::Insertion::insert(arr, i, right);
+        }
+
+        let mut size = Self::RUN;
+        while size < n {
+            for left in (0..n).step_by(2 * size) {
+                let mid = left + size - 1;
+                let right = usize::min(left + 2 * size - 1, n - 1);
+
+                if mid < right {
+                    self.merge.sort(arr, left, mid, right);
+                }
+            }
+
+            size *= 2;
+        }
     }
 }
