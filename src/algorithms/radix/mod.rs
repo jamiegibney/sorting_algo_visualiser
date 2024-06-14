f531f78dab13349998add4f5bc55d3840f5c004a
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-14 04:06:19 +0100

    updated general radix functions

--- src/algorithms/radix/mod.rs
+++ src/algorithms/radix/mod.rs
@@ -3,30 +3,20 @@ use super::*;
 mod lsd;
 mod lsd_in_place;
 mod msd;
-mod msd_in_place;
 
 pub(super) use lsd::RadixLSD;
 pub(super) use lsd_in_place::RadixLSDInPlace;
 pub(super) use msd::RadixMSD;
-pub(super) use msd_in_place::RadixMSDInPlace;
 
-pub(self) const fn get_digit(a: usize, power: usize, radix: usize) -> usize {
-    let x = radix.pow(power as u32);
-    (a / x) % radix
+fn get_digit(a: usize, power: usize, radix: usize) -> usize {
+    let x = (radix as f32).powi(power as i32);
+    (a as f32 / x) as usize % radix
 }
 
-pub(self) fn analyze(
-    arr: &mut SortArray,
-    analysis: &mut Vec<usize>,
-    base: usize,
-) -> usize {
-    analysis.resize(arr.len(), 0);
+fn max_power(arr: &mut SortArray, base: usize) -> usize {
     let mut a = 0;
-
     let log_base = (base as f32).ln();
-
     for i in 0..arr.len() {
-        analysis[i] = 1;
         let log_arr_i = (arr.read(i) as f32).ln();
         let x = (log_arr_i / log_base) as usize;
 
@@ -37,3 +27,5 @@ pub(self) fn analyze(
 
     a
 }
+
+
