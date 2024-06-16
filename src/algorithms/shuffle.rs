b0763a0cc2d487b66a024dc6ba5a7164bd0af319
Jamie Gibney <jamiegibneyuk@gmail.com>
2024-06-16 01:12:44 +0100

    feat: changed shuffle algorithm

--- src/algorithms/shuffle.rs
+++ src/algorithms/shuffle.rs
@@ -5,49 +5,76 @@ use super::*;
 pub struct Shuffle;
 
 impl Shuffle {
-    const ITERS_PER_STEP: usize = 30;
+    const ITERS_PER_STEP: usize = 10;
 
     pub const fn new() -> Self {
         Self
     }
 
-    fn rand_idx(
-        len: usize,
-        win_start: usize,
-        win_size: usize,
-    ) -> (usize, usize) {
-        let rand = |in_window: bool| {
-            let end = if in_window { win_start + win_size } else { len - 1 };
+    fn rand_above(len: usize, start: usize, size: usize) -> (usize, usize) {
+        let rand = |in_win: bool| {
+            let max = if in_win { start + size } else { len }.min(len + 1);
 
-            if end <= win_start {
-                end
+            if start == len - 1 {
+                len - 1
             }
             else {
-                random_range(win_start, end + 1)
+                random_range(start, max)
             }
-            .clamp(0, len - 1)
         };
 
-        (rand(true), rand(false))
+        (rand(true), rand(true))
     }
+
+    fn rand_below(len: usize, start: usize, size: usize) -> (usize, usize) {
+        let rand = |in_win: bool| {
+            let min = if in_win && start >= size { start - size } else { 0 };
+            random_range(min, start)
+        };
+
+        (rand(true), rand(true))
+    }
+
+    // fn rand_idx(
+    //     len: usize,
+    //     win_start: usize,
+    //     win_size: usize,
+    // ) -> (usize, usize) {
+    //     let rand = |in_window: bool| {
+    //         let end = if in_window { win_start + win_size } else { len - 1 };
+    //
+    //         if end <= win_start {
+    //             end
+    //         }
+    //         else {
+    //             random_range(win_start, end + 1)
+    //         }
+    //         .clamp(0, len - 1)
+    //     };
+    //
+    //     (rand(true), rand(false))
+    // }
 }
 
 impl SortProcessor for Shuffle {
     fn process(&mut self, arr: &mut SortArray) {
-        let len = arr.len();
+        let n = arr.len() - 1;
+        let win_size = (n / 4).max(1);
+        let step = 4;
 
-        let mut win_size = len / 40;
-        let mut win_start = 0;
+        let mut head_bot = 0;
+        let mut head_top = arr.len() - 1;
 
-        let step_size = (win_size / 8).max(1);
-
-        while (win_start + win_size) < len {
+        for _ in (0..n * 2).step_by(step) {
             for _ in 0..Self::ITERS_PER_STEP {
-                let (a, b) = Self::rand_idx(len, win_start, win_size);
-                arr.swap(a, b);
+                let (ab, bb) = Self::rand_above(n, head_bot, win_size);
+                let (at, bt) = Self::rand_below(n, head_top, win_size);
+                arr.swap(ab, bt);
+                arr.swap(at, bb);
             }
 
-            win_start += step_size;
+            head_bot = (head_bot + step) % n;
+            head_top = if head_top < step { n - 1 } else { head_top - step };
         }
     }
 }
